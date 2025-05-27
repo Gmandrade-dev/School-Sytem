e6e56ca701ce5b0105aee2b1c8a6e7a8b05e7362
Patrick Kobi <patrickpkttt@gmail.com>
2025-05-26 21:14:47 -0300

    conexao

--- Model/cnx.py
+++ Model/cnx.py
@@ -1,18 +1,18 @@
 import psycopg2
 
 cnx= {
-    'host': 'ballast.proxy.rlwy.net',
-    'dbname': 'railway',
+    'host': 'localhost',
+    'dbname': 'db_escola',
     'user': 'postgres',
-    'password': 'TPuBdQJuTZkbhXrXNmgTXHNFIdMDUHUN',
-    'port': 57581
+    'password': 'admin@',
+    'port': 5432
 }
 
 def conectar():
     try:
         connectando = psycopg2.connect(**cnx)
         connectando.autocommit = True
-        print("ConexÃ£o estabelecida com sucesso!")
+        print("Conexão estabelecida com sucesso!")
         return connectando
     except Exception as e:
         print(f"Erro ao conectar ao banco de dados: {e}")
@@ -24,39 +24,104 @@ def criar_tabela_aluno():
         conexao = conectar()
         cursor = conexao.cursor()
         cursor.execute('''
-            CREATE TABLE IF NOT EXISTS alunos (
-                matricula SERIAL PRIMARY KEY,
-                nome VARCHAR(100),
-                cpf VARCHAR(100),
-                email VARCHAR(100),
-                senha VARCHAR(100)
+            create table IF NOT EXISTS aluno (
+                matricula serial primary key,
+                cpf varchar(14) unique not null,
+                nome varchar(100) not null,
+                email varchar(100) unique not null,
+                senha varchar(100) not null,
+                status boolean default true
             )
         ''')
-        print("Tabela 'alunos' criada com sucesso!")
+        print("Tabela 'aluno' criada com sucesso!")
     except Exception as e:
         print(f"Erro ao criar tabela: {e}")
     finally:
         cursor.close()
 
 
-def criar_tabela_disciplinas():
+def criar_tabela_disciplina():
     try:
         conexao = conectar()
         cursor = conexao.cursor()
         cursor.execute('''
-            CREATE TABLE IF NOT EXISTS disciplinas (
-                id_disciplina SERIAL PRIMARY KEY,
-                nome VARCHAR(100),
-                descricao TEXT
+            create table IF NOT EXISTS disciplina (
+                id_disciplina serial primary key,
+                nome varchar(100) not null,
+                matricula_professor int not null,
+                foreign key (matricula_professor) references professor(matricula) on delete cascade
             )
         ''')
-        print("Tabela 'cursos' criada com sucesso!")
+        print("Tabela 'disciplina' criada com sucesso!")
     except Exception as e:
         print(f"Erro ao criar tabela: {e}")
     finally:
         cursor.close()
 
 
+def criar_tabela_professor():
+    try:
+        conexao = conectar()
+        cursor = conexao.cursor()
+        cursor.execute('''
+            create table IF NOT EXISTS professor (
+                matricula serial primary key,
+                cpf varchar(14) unique not null,
+                nome varchar(100) not null,
+                email varchar(100) unique not null,
+                senha varchar(100) not null,
+                status boolean default true
+            )
+        ''')
+        print("Tabela 'professor' criada com sucesso!")
+    except Exception as e:
+        print(f"Erro ao criar tabela: {e}")
+    finally:
+        cursor.close()
 
 
-# criar_tabela_aluno()
+def criar_tabela_aluno_disciplina():
+    try:
+        conexao = conectar()
+        cursor = conexao.cursor()
+        cursor.execute('''
+            create table IF NOT EXISTS aluno_disciplina (
+                id_aluno_disciplina serial primary key,
+                matricula_aluno int not null,
+                id_disciplina int not null,
+                foreign key (matricula_aluno) references aluno(matricula) on delete cascade,
+                foreign key (id_disciplina) references disciplina(id_disciplina) on delete cascade,
+                unique (matricula_aluno, id_disciplina)
+            )
+        ''')
+        print("Tabela 'aluno_disciplina' criada com sucesso!")
+    except Exception as e:
+        print(f"Erro ao criar tabela: {e}")
+    finally:
+        cursor.close()
+
+
+def criar_tabela_nota():
+    try:
+        conexao = conectar()
+        cursor = conexao.cursor()
+        cursor.execute('''
+            create table IF NOT EXISTS nota (
+                id_nota serial primary key,
+                matricula_aluno int not null,
+                id_disciplina int not null,
+                nota1 numeric(5,2),
+                nota2 numeric(5,2),
+                nota3 numeric(5,2),
+                media numeric(5,2) generated always as 
+                ((coalesce(nota1, 0) + coalesce(nota2, 0) + coalesce(nota3, 0)) / 3) stored,
+                foreign key (matricula_aluno) references aluno(matricula) on delete cascade,
+                foreign key (id_disciplina) references disciplina(id_disciplina) on delete cascade,
+                unique (matricula_aluno, id_disciplina)
+            )
+        ''')
+        print("Tabela 'nota' criada com sucesso!")
+    except Exception as e:
+        print(f"Erro ao criar tabela: {e}")
+    finally:
+        cursor.close()
--- conexao.py
+++ conexao.py
@@ -34,7 +34,135 @@ finally:
    if conexao_inicial:
       conexao_inicial.close()
 
-# Etapa 2: Conecta ao banco de dados 'db_escola' para criar a tabela
+# Etapa 2: Conecta ao banco de dados 'db_escola' para criar as tabelas
+
+cnx= {
+    'host': 'localhost',
+    'dbname': 'db_escola',
+    'user': 'postgres',
+    'password': 'admin@',
+    'port': 5432
+}
+
+def conectar():
+    try:
+        connectando = psycopg2.connect(**cnx)
+        connectando.autocommit = True
+        print("Conexão estabelecida com sucesso!")
+        return connectando
+    except Exception as e:
+        print(f"Erro ao conectar ao banco de dados: {e}")
+        return None
+
+
+def criar_tabela_aluno():
+    try:
+        conexao = conectar()
+        cursor = conexao.cursor()
+        cursor.execute('''
+            create table IF NOT EXISTS aluno (
+                matricula serial primary key,
+                cpf varchar(14) unique not null,
+                nome varchar(100) not null,
+                email varchar(100) unique not null,
+                senha varchar(100) not null,
+                status boolean default true
+            )
+        ''')
+        print("Tabela 'aluno' criada com sucesso!")
+    except Exception as e:
+        print(f"Erro ao criar tabela: {e}")
+    finally:
+        cursor.close()
+
+
+def criar_tabela_disciplina():
+    try:
+        conexao = conectar()
+        cursor = conexao.cursor()
+        cursor.execute('''
+            create table IF NOT EXISTS disciplina (
+                id_disciplina serial primary key,
+                nome varchar(100) not null,
+                matricula_professor int not null,
+                foreign key (matricula_professor) references professor(matricula) on delete cascade
+            )
+        ''')
+        print("Tabela 'disciplina' criada com sucesso!")
+    except Exception as e:
+        print(f"Erro ao criar tabela: {e}")
+    finally:
+        cursor.close()
+
+
+def criar_tabela_professor():
+    try:
+        conexao = conectar()
+        cursor = conexao.cursor()
+        cursor.execute('''
+            create table IF NOT EXISTS professor (
+                matricula serial primary key,
+                cpf varchar(14) unique not null,
+                nome varchar(100) not null,
+                email varchar(100) unique not null,
+                senha varchar(100) not null,
+                status boolean default true
+            )
+        ''')
+        print("Tabela 'professor' criada com sucesso!")
+    except Exception as e:
+        print(f"Erro ao criar tabela: {e}")
+    finally:
+        cursor.close()
+
+
+def criar_tabela_aluno_disciplina():
+    try:
+        conexao = conectar()
+        cursor = conexao.cursor()
+        cursor.execute('''
+            create table IF NOT EXISTS aluno_disciplina (
+                id_aluno_disciplina serial primary key,
+                matricula_aluno int not null,
+                id_disciplina int not null,
+                foreign key (matricula_aluno) references aluno(matricula) on delete cascade,
+                foreign key (id_disciplina) references disciplina(id_disciplina) on delete cascade,
+                unique (matricula_aluno, id_disciplina)
+            )
+        ''')
+        print("Tabela 'aluno_disciplina' criada com sucesso!")
+    except Exception as e:
+        print(f"Erro ao criar tabela: {e}")
+    finally:
+        cursor.close()
+
+
+def criar_tabela_nota():
+    try:
+        conexao = conectar()
+        cursor = conexao.cursor()
+        cursor.execute('''
+            create table IF NOT EXISTS nota (
+                id_nota serial primary key,
+                matricula_aluno int not null,
+                id_disciplina int not null,
+                nota1 numeric(5,2),
+                nota2 numeric(5,2),
+                nota3 numeric(5,2),
+                media numeric(5,2) generated always as 
+                ((coalesce(nota1, 0) + coalesce(nota2, 0) + coalesce(nota3, 0)) / 3) stored,
+                foreign key (matricula_aluno) references aluno(matricula) on delete cascade,
+                foreign key (id_disciplina) references disciplina(id_disciplina) on delete cascade,
+                unique (matricula_aluno, id_disciplina)
+            )
+        ''')
+        print("Tabela 'nota' criada com sucesso!")
+    except Exception as e:
+        print(f"Erro ao criar tabela: {e}")
+    finally:
+        cursor.close()  
+
+####
 
 
 
